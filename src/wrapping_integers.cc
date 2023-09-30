d3b683e20785f790fb226cced1958e020f3d7571
EternalSamsara <lrytrying@gmail.com>
2023-09-30 23:18:34 +0800

    check2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -15,17 +15,17 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   // Your code here.
   (void)zero_point;
   (void)checkpoint;
-  uint64_t abs_seque = static_cast<uint64_t>(this->raw_value_ - zero_point.raw_value_);
-  uint64_t mod = (checkpoint - abs_seque) >> 32;
-  uint64_t remainder = ((checkpoint - abs_seque) << 32) >> 32;
+  uint64_t abs_seqno = static_cast<uint64_t>(this->raw_value_ - zero_point.raw_value_);
+  uint64_t mod = (checkpoint - abs_seqno) >> 32;
+  uint64_t remainder = ((checkpoint - abs_seqno) << 32) >> 32;
 
-  if(checkpoint < abs_seque){
-    return abs_seque;
+  if(checkpoint < abs_seqno){
+    return abs_seqno;
   }
   else if(remainder < (1UL << 31)){
-    return abs_seque + (mod << 32);
+    return abs_seqno + (mod << 32);
   }
   else{
-    return abs_seque + ((mod + 1) << 32);
+    return abs_seqno + ((mod + 1) << 32);
   }
 }
